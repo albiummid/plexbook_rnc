3922c27fb7c8a005706f4fcd583f5e0238bcdbdf
albiummid <albi.ummid@gmail.com>
2024-08-14 22:38:52 +0600

    package init

--- src/screens/contents/movies/index.tsx
+++ src/screens/contents/movies/index.tsx
@@ -1,10 +1,7 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import tw from '../../../lib/tailwind';
 
 export default function MoviesHomeScreen() {
-  return (
-    <View>
-      <Text>MovieScreen</Text>
-    </View>
-  );
+  return <View style={tw` flex-1 bg-red-300`}></View>;
 }
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,10 +1,32 @@
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import React from 'react';
+import TabView from '../components/ui/TabView';
+import MoviesHomeScreen from './contents/movies';
+import SeriesHomeScreen from './contents/series';
+import tw from '../lib/tailwind';
+import ActorHomeScreen from './contents/actor';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={tw`flex-1 `}>
+      <TabView
+        // stickyTab={true}
+        textStyle={tw``}
+        tabItems={[
+          {
+            name: 'Movies',
+            component: MoviesHomeScreen,
+          },
+          {
+            name: 'Series',
+            component: SeriesHomeScreen,
+          },
+          {
+            name: 'Actors',
+            component: ActorHomeScreen,
+          },
+        ]}
+      />
     </View>
   );
 }
