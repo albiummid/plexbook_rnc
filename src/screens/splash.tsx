d9979d51ea2ead6e99ab1c4b3f13368217e0e83f
albiummid <albi.ummid@gmail.com>
2024-09-03 14:33:12 +0600

    splash screen updated

--- src/screens/splash.tsx
+++ src/screens/splash.tsx
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react';
-import {Image, ImageBackground} from 'react-native';
+import {Image} from 'react-native';
+import TText from '../components/ui/TText';
+import TView from '../components/ui/TView';
 import {useFirebaseAuth} from '../libs/firebase';
 import tw from '../libs/tailwind';
 import {getMS, wait} from '../libs/utils/helpers';
@@ -11,7 +13,7 @@ export default function SplashScreen() {
   console.log(isAuthenticated);
   useEffect(() => {
     if (!isLoading) {
-      wait(getMS.second(100)).then(() => {
+      wait(getMS.second(4)).then(() => {
         if (isAuthenticated) {
           router.replace('tab_root');
         } else {
@@ -22,13 +24,23 @@ export default function SplashScreen() {
   }, [isAuthenticated, isLoading]);
 
   return (
-    <ImageBackground
-      source={require('../assets/images/movie-bg.png')}
-      style={tw`flex-1 bg-black justify-center items-center`}>
-      <Image
-        style={tw`h-60 w-60`}
-        source={require('../assets/images/logo.png')}
-      />
-    </ImageBackground>
+    <TView
+      // source={require('../assets/images/movie-bg.png')}
+      style={tw`flex-1 bg-black  `}>
+      <TView style={tw`justify-center flex-1 items-center`}>
+        <Image
+          style={tw`h-60 w-60`}
+          source={require('../assets/images/logo.png')}
+        />
+      </TView>
+      <TView>
+        <TText style={tw`text-white mt-auto mx-auto`}>
+          Made with ❤️ by Albi Ummid
+        </TText>
+        <TText style={tw`text-white text-xs mt-auto mx-auto`}>
+          albiummid@gmail.com
+        </TText>
+      </TView>
+    </TView>
   );
 }
