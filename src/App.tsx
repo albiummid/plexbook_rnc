74d2c620c3a3427063d128ba83d32e29522a8723
albiummid <albi.ummid@gmail.com>
2024-09-03 23:01:27 +0600

    App  update support enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Alert, Linking} from 'react-native';
+import {config} from '../app.config';
 import ProviderWrapper from './components/layout/ProviderWrapper';
 import './components/ui/ActionSheet';
+import {api} from './libs/api';
 import {RootNavigator} from './libs/navigation/Screens';
-
+const downloadLink =
+  'https://github.com/Zenda-Cross/vega-app/releases/download/v2.4.1/Vega-armeabi-v7a-v2.4.1.apk';
 export default function InitApp() {
+  useEffect(() => {
+    api.post('/auth/version/check', {version: config.version}).then(data => {
+      if (data.data.result.hasUpdate) {
+        Alert.alert(
+          'Update',
+          `App version v${data.data.result.version} released.\nDownload and update this app`,
+          [
+            {
+              text: 'Open download link',
+              onPress: () => {
+                Linking.openURL(data.data.result.downloadURL);
+              },
+            },
+          ],
+        );
+      }
+    });
+  }, []);
   return (
     <ProviderWrapper>
       <RootNavigator />
